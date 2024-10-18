98709030e009d11c871262c4bd55376e3fa6c198
PandaSaimon <git@stackblitz.com>
2024-10-19 00:05:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# zfxg2v
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/PandaSaimon/zfxg2v)
---
+++ src/example/button-toggle-appearance-example.ts
@@ -0,0 +1,48 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+
+/**
+ * @title Button toggle appearance
+ */
+@Component({
+  selector: 'button-toggle-appearance-example',
+  templateUrl: 'button-toggle-appearance-example.html',
+  styleUrl: 'button-toggle-appearance-example.css',
+  standalone: true,
+  imports: [
+    MatButton,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    MatTabsModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonToggleAppearanceExample {
+  owners = new FormControl('');
+
+  ownerList: string[] = [
+    'Ответственный 1',
+    'Ответственный 2',
+    'Ответственный 3',
+    'Ответственный 4',
+    'Ответственный 5',
+  ];
+}
+
+/**  Copyright 2024 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
