28aef79b47ddeae6f942121c3c48ee39afecf224
damir6608 <latypovdamir3@gmail.com>
2025-09-16 16:28:21 +0300

    CRM: правки замечаний

--- src/example/button-toggle-appearance-example.ts
+++ src/example/button-toggle-appearance-example.ts
@@ -18,6 +18,8 @@ import {MatTableModule} from '@angular/material/table';
 import { ArticleCalc } from './article-calc/article-calc';
 import { CommonModule } from '@angular/common';
 
+import { MatBadgeModule } from '@angular/material/badge';
+
 /**
  * @title Button toggle appearance
  */
@@ -44,39 +46,48 @@ import { CommonModule } from '@angular/common';
     MatButtonToggleModule,
     MatTableModule,
     ArticleCalc,
-    CommonModule
+    CommonModule,
+    MatBadgeModule
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonToggleAppearanceExample {
   saveMode: string = 'saveSelected';
-  displayedColumns: string[] = ['sale', 'product', 'oldSum', 'sum'];
+  displayedColumns: string[] = ['sale', 'product', 'project', 'oldSum', 'sum'];
 
   summaryParams: SummaryParams[] = [
-    {sale: 'Продажа 1', product: 'SIEM', oldSum: '34 697,00', sum: '74 542,00'},
-    {sale: 'Продажа 2', product: 'NGFW', oldSum: '25 697,00', sum: '25 542,00'},
-    {sale: 'Продажа 3', product: 'NAD', oldSum: '69 697,00', sum: '63 542,00'},
-    {sale: 'Продажа 4', product: 'MaxPatrol 8', oldSum: '85 697,00', sum: '36 542,00'},
+    {sale: 'Продажа 1', product: 'SIEM', project: 'Проект 1', oldSum: '34 697,00', sum: '74 542,00', isBadge: false},
+    {sale: 'Продажа 2', product: 'NGFW', project: 'Проект 2', oldSum: '25 697,00', sum: '25 542,00', isBadge: true},
+    {sale: 'Продажа 3', product: 'NAD', project: 'Проект 3', oldSum: '69 697,00', sum: '63 542,00', isBadge: true},
+    {sale: 'Продажа 4', product: 'MaxPatrol 8', project: 'Проект 4', oldSum: '85 697,00', sum: '36 542,00', isBadge: false},
   ];
 
   specCalcs: SpecCalc[] = [
-    { id: 'fe2b5aec-c5db-450c-b2ba-bd6782423d6f', name: 'Расчет спецификации 1', product: 'SIEM', sum: '6 985,00', isSelected: false},
-    { id: '260e7067-53d8-4b63-bfb5-ef6def0e6f0b', name: 'Расчет спецификации 2', product: 'NGFW', sum: '85 274,00', isSelected: false},
-    { id: '6e9aaf9e-1b64-45c7-bd9d-e59ddccb2d35', name: 'Расчет спецификации 3', product: 'NAD', sum: '9 652,00', isSelected: false},
-    { id: 'f64453cb-316a-4dd8-9f24-ad63c30a2480', name: 'Расчет спецификации 4', product: 'MaxPatrol 8', sum: '5 285,00', isSelected: false},
-    { id: '62154d1c-9705-4343-99c8-d1180315f603', name: 'Расчет спецификации 5', product: 'PT BlackBox', sum: '367 485,00', isSelected: false},
-    { id: 'a70ead1c-0517-4b63-be1a-ab2bb3c73389', name: 'Расчет спецификации 6', product: 'NGFW', sum: '9 985,00', isSelected: false},
+    { id: 'fe2b5aec-c5db-450c-b2ba-bd6782423d6f', name: 'Расчет спецификации 1', product: 'SIEM', sum: '6 985,00', sumTP: '236,15', otherSum: '5 589,55', isSelected: false},
+    { id: '260e7067-53d8-4b63-bfb5-ef6def0e6f0b', name: 'Расчет спецификации 2', product: 'NGFW', sum: '85 274,00', sumTP: '864,15', otherSum: '5 655,55', isSelected: false},
+    { id: '6e9aaf9e-1b64-45c7-bd9d-e59ddccb2d35', name: 'Расчет спецификации 3', product: 'NAD', sum: '9 652,00', sumTP: '854,15', otherSum: '8 541,55', isSelected: false},
+    { id: 'f64453cb-316a-4dd8-9f24-ad63c30a2480', name: 'Расчет спецификации 4', product: 'MaxPatrol 8', sum: '5 285,00', sumTP: '368,15', otherSum: '6 241,55', isSelected: false},
+    { id: '62154d1c-9705-4343-99c8-d1180315f603', name: 'Расчет спецификации 5', product: 'PT BlackBox', sum: '367 485,00', sumTP: '120,15', otherSum: '8 341,55', isSelected: false},
+    { id: 'a70ead1c-0517-4b63-be1a-ab2bb3c73389', name: 'Расчет спецификации 6', product: 'NGFW', sum: '9 985,00', sumTP: '856,15', otherSum: '9 145,55', isSelected: false},
   ]
 
   specCalcs1: SpecCalc[] = [
-    { id: 'f64453cb-316a-4dd8-9f24-ad63c30a2480', name: 'Расчет спецификации 1', product: 'MaxPatrol 8', sum: '5 285,00', isSelected: false},
-    { id: '62154d1c-9705-4343-99c8-d1180315f603', name: 'Расчет спецификации 2', product: 'PT BlackBox', sum: '367 485,00', isSelected: false},
-    { id: 'a70ead1c-0517-4b63-be1a-ab2bb3c73389', name: 'Расчет спецификации 3', product: 'NGFW', sum: '9 985,00', isSelected: false},
+    { id: 'f64453cb-316a-4dd8-9f24-ad63c30a2480', name: 'Расчет спецификации 1', product: 'MaxPatrol 8', sum: '5 285,00', sumTP: '596,15', otherSum: '7 589,55', isSelected: false},
+    { id: '62154d1c-9705-4343-99c8-d1180315f603', name: 'Расчет спецификации 2', product: 'PT BlackBox', sum: '367 485,00', sumTP: '8 569,33', otherSum: '968,33',  isSelected: false},
+    { id: 'a70ead1c-0517-4b63-be1a-ab2bb3c73389', name: 'Расчет спецификации 3', product: 'NGFW', sum: '9 985,00', sumTP: '258 796,00', otherSum: '52,22',  isSelected: false},
   ]
 
   /** Gets the total cost of all transactions. */
   getTotalCost() {
     return "9 815 687,57";
+  }
+
+  validateSale(summaryParam: SummaryParams): boolean {
+    return !summaryParam.isBadge;
+  }
+
+  getSpecificValue(specCalc: SpecCalc): string {
+    return specCalc.product === 'NGFW' ? `Сумма оборудования: ${specCalc.otherSum}` : `Сумма услуги: ${specCalc.otherSum}`;
   }
 
   owners = new FormControl('');
@@ -93,8 +104,10 @@ export class ButtonToggleAppearanceExample {
 export interface SummaryParams {
   sale: string,
   product: string,
+  project: string,
   oldSum: string,
-  sum: string
+  sum: string,
+  isBadge: boolean
 }
 
 export interface SpecCalc {
@@ -102,6 +115,8 @@ export interface SpecCalc {
   name: string,
   product: string,
   sum: string,
+  sumTP: string,
+  otherSum: string,
   isSelected: boolean
 }
 
